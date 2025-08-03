0fa36520967202312842889ae1d3cfd855ec874c
ZHNEC77 <vadim030714@gmail.com>
2025-08-03 15:22:10 +0300

    first commit

---
+++ backend/chat/consumers.py
@@ -0,0 +1,11 @@
+import json
+from channels.generic.websocket import AsyncWebsocketConsumer
+from channels.db import database_sync_to_async
+from .models import Room, Message
+
+
+class ChatConsumer(AsyncWebsocketConsumer):
+    async def connect(self):
+        self.room_name = f"room_{self.scope['url_route']['kwargs']['room_name']}"
+        await self.channel_layer.group_add(self.room_name, self.channel_name)
+        await
