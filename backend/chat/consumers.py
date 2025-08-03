d797ae5e1cdfc10179ec762e173bfbb8b17b3389
ZHNEC77 <vadim030714@gmail.com>
2025-08-03 15:52:40 +0300

    add asgi

--- backend/chat/consumers.py
+++ backend/chat/consumers.py
@@ -5,7 +5,13 @@ from .models import Room, Message
 
 
 class ChatConsumer(AsyncWebsocketConsumer):
+
     async def connect(self):
         self.room_name = f"room_{self.scope['url_route']['kwargs']['room_name']}"
         await self.channel_layer.group_add(self.room_name, self.channel_name)
-        await
+        await self.accept()
+
+    async def disconnect(self, code):
+        self.room_name = f"room_{self.scope['url_route']['kwargs']['room_name']}"
+        await self.channel_layer.group_discard(self.room_name, self.channel_name)
+        self.close(code)
